ed55c5b6dec15ff8be597eebb52f9fa78b63c090
Matt Lucas <mattlucas@gmail.com>
2014-03-12 13:59:13 -0400

    loop holes

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -92,13 +92,16 @@ App.makeMap = function() {
     // pick list. Retrieve the matching places for that item.
     google.maps.event.addListener(searchBox, 'places_changed', function() {
       var places = searchBox.getPlaces();
+      // The places var is an array of things that will get placed by markers
+      console.log(places);
+      // loop through this to see what I'm getting
       for (var i = 0, marker; i < markers.length; i++){
         marker = markers[i];
         marker.setMap(null);
     }
 
     // For each place, get the icon, place name, and location.
-    // markers = [];
+
     var bounds = new google.maps.LatLngBounds();
     for (var i = 0, place; place = places[i]; i++) {
       var image = {
@@ -131,7 +134,10 @@ App.makeMap = function() {
         // bias to nearby results
       });
 
+      // console.log(markers);
+      // console.log(places);
       markers.push(marker);
+
 
       bounds.extend(place.geometry.location);
     }
